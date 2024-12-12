8cf9439adf46857aa5c60141a1ff17d535152df1
x64 <okeowoaderemi@gmail.com>
2024-12-11 22:02:05 -0500

    * Added Support for Tailwind + TypeScript

---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.liquid', './src/**/*.{ts,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: 'var(--font-body-family)',
+        heading: 'var(--font-heading-family)',
+      },
+      colors: {
+        text: 'rgba(var(--color-base-text), <alpha-value>)',
+        accent1: 'rgba(var(--color-base-accent-1), <alpha-value>)',
+        accent2: 'rgba(var(--color-base-accent-2), <alpha-value>)',
+        background1: 'rgba(var(--color-base-background-1), <alpha-value>)',
+        background2: 'rgba(var(--color-base-background-2), <alpha-value>)',
+      },
+    },
+  },
+  plugins: [],
+};
